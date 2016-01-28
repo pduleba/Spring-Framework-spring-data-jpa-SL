26d64caf8aa54144f452d6d27e05c7cab619b46e
pduleba <pduleba@pgs-soft.com>
2016-01-28 10:03:21 +0100

    Changes

--- ps-guitar-db/src/test/java/com/guitar/db/LocationPersistenceTests.java
+++ ps-guitar-db/src/test/java/com/guitar/db/LocationPersistenceTests.java
@@ -14,7 +14,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.util.Assert;
 
 import com.guitar.db.model.Location;
 import com.guitar.db.repository.LocationJpaRepository;
