6c8bbede784670b8ac4b60578c88f9c77425bc2b
pduleba <pduleba@pgs-soft.com>
2016-01-29 13:25:26 +0100

    Clean

--- ps-guitar-db/src/main/java/com/guitar/db/repository/LocationJpaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.guitar.db.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.guitar.db.model.Location;
-
-public interface LocationJpaRepository extends JpaRepository<Location, Long> {
-
-}
