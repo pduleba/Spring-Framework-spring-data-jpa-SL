abd8db4e415d42fb6a5393b141a71d3224a3e31a
pduleba <pduleba@pgs-soft.com>
2016-02-01 14:49:59 +0100

    Changes

--- spring-data-jpa/src/main/java/com/pduleba/spring/data/dao/OwnerDao.java
+++ spring-data-jpa/src/main/java/com/pduleba/spring/data/dao/OwnerDao.java
@@ -20,34 +20,4 @@ public interface OwnerDao extends JpaRepository<OwnerModel, Long> {
 
 	List<OwnerModel> getByFirstName(String firstName);
 
-	List<OwnerModel> getByFirstNameLikeOrLastNameLike(String firstName, String lastName);
-
-	List<OwnerModel> getByFirstNameIsAndLastNameEqualsAndAgeNot(String firstName, String lastName, Integer age);
-
-	List<OwnerModel> getByFirstNameNotLike(String firstName);
-
-	List<OwnerModel> getByFirstNameStartingWithOrFirstNameEndingWithOrFirstNameContaining(String firstNameStartingWith,
-			String firstNameEndingWith, String firstNameContaining);
-	
-	List<OwnerModel> getByAgeLessThanEqualAndAgeGreaterThan(int ageLessThanEqual, int ageGreaterThan);
-
-	List<OwnerModel> getByAgeBeforeAndAgeAfterAndAgeBetween(int i, int j, int k, int l);
-	
-	List<OwnerModel> getByActiveTrue();
-	
-	List<OwnerModel> getByAgeIsNotNullOrActiveIsNull();
-	
-	List<OwnerModel> getByAgeInAndActiveNotIn(List<Integer> in, List<Boolean> notIn);
-	
-	List<OwnerModel> getByCarsNameIn(List<String> in);
-	
-	List<OwnerModel> getByFirstNameIgnoreCaseAndLastName(String firstNameIgnoreCase, String lastName);
-
-	List<OwnerModel> getByActiveOrderByFirstNameDesc(Boolean active);
-	
-	List<OwnerModel> findFirstByActiveTrueOrderByFirstNameAsc();
-	
-	List<OwnerModel> findTop2ByActiveTrueOrderByFirstNameAsc();
-
-	List<OwnerModel> findDistinctByActiveTrueOrderByFirstNameAsc();
 }
