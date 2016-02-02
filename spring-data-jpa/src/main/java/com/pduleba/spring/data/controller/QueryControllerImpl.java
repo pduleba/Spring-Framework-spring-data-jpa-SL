1c3f1fe32ca110b34a948214334424df5cac766c
pduleba <pduleba@pgs-soft.com>
2016-02-02 08:45:40 +0100

    Changes

--- spring-data-jpa/src/main/java/com/pduleba/spring/data/controller/QueryControllerImpl.java
+++ spring-data-jpa/src/main/java/com/pduleba/spring/data/controller/QueryControllerImpl.java
@@ -1,10 +1,13 @@
 package com.pduleba.spring.data.controller;
 
+import java.util.List;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import com.pduleba.jpa.model.OwnerModel;
 import com.pduleba.spring.data.services.OwnerService;
 import com.pduleba.spring.data.services.UtilityService;
 
@@ -24,10 +27,17 @@ public @Data class QueryControllerImpl implements QueryController {
 	public void executeQueries() {
 		LOG.info("----------- Execute Auditing -----------");
 		LOG.info("########### SELECT ###########");
-		utils.show(ownerSerivce.getByFirstName("Darek"));
+		List<OwnerModel> list = ownerSerivce.getByFirstName("Darek");
+		utils.show(list);
 		
-		LOG.info("########### UPDATE ###########");
-		utils.show(ownerSerivce.updateLastNameByFirstName("DD", "Darek"));
+		LOG.info("########### UPDATE - DOES NOT WORK - update query ###########");
+		utils.show(ownerSerivce.updateLastNameByFirstName("DD", "Darek"));		
+		
+		LOG.info("########### UPDATE - WORKS ###########");
+		OwnerModel entity = ownerSerivce.getById(list.get(0).getId());
+		entity.setLastName("DDD");
+		ownerSerivce.create(entity);
+		
+		LOG.info("----------- Complete -----------");
 	}
-
 }
